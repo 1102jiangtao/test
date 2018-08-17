d5b29b728b669b8669155caba2578f3843cda312
1102jiangtao <876474191@qq.com>
2018-08-17 21:32:22 +0800

    Revert "11"
    This reverts commit d09bf826a4d1f885da04deb7d5918e4e111fc180.

--- test-github/src/Test.java
+++ test-github/src/Test.java
@@ -3,6 +3,5 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test.main()cesh1i");
 		System.out.println("Test.main()33333www");
-		System.out.println("wwww");
 	}
 }
