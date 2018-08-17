0d479f05cb05a1eebf88766ffd2ed5cadaadfb2b
1102jiangtao <876474191@qq.com>
2018-08-17 22:07:20 +0800

    a

--- test-github/src/Test.java
+++ test-github/src/Test.java
@@ -1,10 +1,6 @@
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Test.main()cesh1i");
-		System.out.println("Test.main()33333www");
-		System.out.println("wwww1");
-		System.out.println("x1");
-		System.out.println("wwwwww221");
+		String a = "";
 	}
 }
